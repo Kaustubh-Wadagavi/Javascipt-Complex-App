7b6402ad5d3c0a2462cc0745e04b4b78d7ed4718
Kaustubh Wadagavi <kaustubhwadagavi@gmail.com>
2020-05-15 17:37:58 +0530

    Added Login page and Logout feature

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,14 +3,22 @@ const User = require('../models/User')
 exports.login = function(req, res) {
     let user = new User(req.body)
     user.login().then(function(result) {
-        res.send(result)
+        req.session.user = {favColor: "blue", username: user.data.username}
+        req.session.save(function() {
+        res.redirect('/')
+        })
     }).catch(function(e) {
-        res.send(e)
+        req.flash('errors', e)
+        req.session.save(function(){
+            res.redirect('/')
+        })
     })
 }
 
-exports.logout = function() {
-    
+exports.logout = function(req, res) {
+    req.session.destroy(function() {
+        res.redirect('/')
+    })
 }
 
 exports.register = function(req, res) {
@@ -24,5 +32,9 @@ exports.register = function(req, res) {
 }
 
 exports.home = function(req, res) {
-    res.render('home-guest')
+   if (req.session.user) {
+    res.render('home-dashboard',{username: req.session.user.username})
+   } else {
+    res.render('home-guest', {errors: req.flash('errors')})
+   }
 }
